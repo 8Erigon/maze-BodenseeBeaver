7c44d040e6d83ec8194731072ada68c798ab53d9
Erik Bergmann <lese.erik@gmail.com>
2024-10-16 17:33:41 +0200

    Wrote Comment for MovementConstructor

--- lib/Motors/Movement/Movement.cpp
+++ lib/Motors/Movement/Movement.cpp
@@ -1,5 +1,5 @@
 #include "Movement.h"
 
-Movement::Movement(Motor _motor[4]): motor({_motor[1], _motor[2], _motor[3], _motor[4]}){
+Movement::Movement(Motor _motor[4]): motor({_motor[1], _motor[2], _motor[3], _motor[4]}){ //Benutzt "Member Initializer List"-Technik
 
 }

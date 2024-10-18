b71015d53a19d5f65f205b7a112fe627ec4d30b3
Erik Bergmann <lese.erik@gmail.com>
2024-10-18 16:30:47 +0200

    added vektors.h

---
+++ lib/MatrixVector/vektors.h
@@ -0,0 +1,19 @@
+#ifndef vektors_h
+#define vektors_h
+
+template <typename type> class vektors{
+    public: 
+    vektors(int lenght){
+        content = new type[lenght];
+    };
+    ~vektors(){
+        free(content);
+    };
+    type content[];
+
+
+};
+
+
+
+#endif

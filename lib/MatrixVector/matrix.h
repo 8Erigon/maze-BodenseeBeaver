fea36b89848aa8e202e597433ef0c9899f1c9988
Erik Bergmann <lese.erik@gmail.com>
2024-10-20 20:26:09 +0200

    added "matrix * vektors"

--- lib/MatrixVector/matrix.h
+++ lib/MatrixVector/matrix.h
@@ -20,6 +20,18 @@ template <typename type, int lenght, int width> class matrix{
         }
         return result;
     }
+
+    vektors<type, lenght> operator*(const vektors &other){ //function for when you do "matrix * vektors"
+        vektors<type, lenght> result;
+        matrix<double, lenght, width> preResult;
+        for(int iLenght; iLenght<lenght; iLenght++){
+            for(int iWidth; iWidth<width; iWidth++){
+                preResult.colums[iWidth] = colums[iWidth] * other;
+                result.content[iLenght] = result.content[iLenght] + preResult.colums[iWidth].content[iLenght];
+            }
+        }
+        return result;
+    }
 };
 
 #endif

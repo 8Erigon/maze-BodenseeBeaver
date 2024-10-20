21607592f9c81ccb8a1f1b4280ee20f87f5db161
Erik Bergmann <lese.erik@gmail.com>
2024-10-20 13:00:06 +0200

    added matrix. operator of vektors class doesn't change itself anymore

--- lib/MatrixVector/vektors.h
+++ lib/MatrixVector/vektors.h
@@ -3,35 +3,39 @@
 
 template <typename type, int lenght> class vektors{
     #if (type != int || type != long || type != float || type != double) //throw error if type is not a number when compiling
-    #error "only int allow"
+    #error "the class 'vektors' only allows a number type like int."
     #endif
     public: 
     type content[lenght];
 
     vektors<type, lenght> operator*(const int &other){ //function for when you do "vektor * int"
+        vektors<type, lenght> result;
         for(int i; i<lenght; i++){
-            content[i] *= other;
+            result.content[i] = content[i] * other;
         }
-        return this;
+        return result;
     }
     vektors<type, lenght> operator/(const int &other){ //function for when you do "vektor / int"
+        vektors<type, lenght> result;
         for(int i; i<lenght; i++){
-            content[i] /= other;
+            result.content[i] = content[i] = other;
         }
-        return this;
+        return result;
     }
 
     vektors<type, lenght> operator+(const vektors &other){ //function for when you do "vektor + vektor"
+        vektors<type, lenght> result;
         for(int i; i<lenght; i++){
-            content[i] += other.content[i];
+            result[i] = content[i] + other.content[i];
         }
-        return this;
+        return result;
     }
     vektors<type, lenght> operator-(const vektors &other){ //function for when you do "vektor - vektor"
+        vektors<type, lenght> result;
         for(int i; i<lenght; i++){
-            content[i] -= other.content[i];
+            result[i] = content[i] - other.content[i];
         }
-        return this;
+        return result;
     }
 };
 

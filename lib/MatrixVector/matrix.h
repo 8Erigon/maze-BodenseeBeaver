4fc63c4bb3b452f32509276e082a9b2437a925d3
Erik Bergmann <lese.erik@gmail.com>
2024-10-20 13:21:30 +0200

    changed comment

--- lib/MatrixVector/matrix.h
+++ lib/MatrixVector/matrix.h
@@ -6,14 +6,14 @@
 template <typename type, int lenght, int width> class matrix{
     vektors<type, lenght> colums[width];
 
-    matrix<type, lenght, width> operator*(const int &other){ //function for when you do "vektor * int"
+    matrix<type, lenght, width> operator*(const int &other){ //function for when you do "matrix * int"
         matrix<type, lenght, width> result;
         for(int i; i<width; i++){
             result.colums[i] = colums[i] * other;
         }
         return result;
     }
-    matrix<type, lenght, width> operator/(const int &other){ //function for when you do "vektor / int"
+    matrix<type, lenght, width> operator/(const int &other){ //function for when you do "matrix / int"
         matrix<type, lenght, width> result;
         for(int i; i<width; i++){
             result.colums[i] = colums[i] / other;

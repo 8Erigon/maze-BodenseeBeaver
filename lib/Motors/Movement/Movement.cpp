e51883e61cb47129614406b105a84f5c14705858
Erik Bergmann <lese.erik@gmail.com>
2024-10-28 00:07:06 +0100

    changed moevement.forward() to movement.speed small naming & comment changes

--- lib/Motors/Movement/Movement.cpp
+++ lib/Motors/Movement/Movement.cpp
@@ -1,13 +1,7 @@
 #include "Movement.h"
 
 Movement::Movement(Motor &_motor, sensors_event_t &gyro): motors(&_motor){ //Benutzt "Member Initializer List"-Technik
-    rotation = &gyro;
-}
-
-void Movement::forward(int speed){
-    for(int i = 0; i<4; i++){
-        motors[i].speed = speed;
-    }
+    orientation = &gyro;
 }
 
 void Movement::process(){
--- lib/Motors/Movement/Movement.h
+++ lib/Motors/Movement/Movement.h
@@ -18,20 +18,20 @@ public:
     @brief drive the roboter forward or backwards with a speed upto 255
     @param speed from -255 to 255; 
     */
-    void forward(int speed);
-    /*
-    @brief Processes Motors&Movement. Should be used at the start of each loop
-    */
-    void process();
+    short speed = 0;
     /*
     @brief Angle the robot should turn to in the end.
     */
     ushort goalAngle = 0;
+    /*
+    @brief Processes Motors&Movement. Should be used at the start of each loop
+    */
+    void process();
 private:
     /*
     @brief pointer to a sensor_event_t holding orientation data of a BNO 
     */
-    sensors_event_t *rotation;
+    sensors_event_t *orientation;
     /*
     @brief Pointer to array of 4 motors. Just handle it like a normal array.
     @example motor[0].speed = 0;

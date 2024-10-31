c31773a43ba3a258902ac249d855683dcf6d254d
Erik Bergmann <lese.erik@gmail.com>
2024-10-31 19:26:02 +0100

    first main.cpp edit

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,21 @@
 #include "../lib/General/General.h"
 #include "../lib/MatrixVector/vektors.h"
 // put function declarations here:
-
+Robot robo;
 
 void setup() {
   // put your setup code here, to run once:
-  
+  robo.move.speed = 10;
+  robo.move.goalAngle = 180;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(robo.readSwitch(StartSwitch)){
+    robo.process();
+  } else {
+    robo.setRunning(false);
+  }
 }
 
 // put function definitions here:

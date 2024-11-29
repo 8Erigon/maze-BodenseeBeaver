9fd71ed7dfa1d340d26e223605a90380b6bd4684
Erik Bergmann <lese.erik@gmail.com>
2024-11-29 17:16:45 +0100

    Moved defines anf enums from general.h to new file

---
+++ lib/General/DefineEnum.h
@@ -0,0 +1,70 @@
+//Motors
+#define MOTOR_FREQUENCY 150
+
+//Mo2 bei Platine
+#define MOTOR_BACK_LEFT_IN1 2
+#define MOTOR_BACK_LEFT_IN2 3
+#define MOTOR_BACK_LEFT_PWM 5
+#define MOTOR_BACK_LEFT_FACTOR -1
+
+//Mo4  bei Platine
+#define MOTOR_BACK_RIGHT_IN1 6
+#define MOTOR_BACK_RIGHT_IN2 7
+#define MOTOR_BACK_RIGHT_PWM 9
+#define MOTOR_BACK_RIGHT_FACTOR 5
+
+//Mo3 bei Platine
+#define MOTOR_FRONT_LEFT_IN1 4
+#define MOTOR_FRONT_LEFT_IN2 5
+#define MOTOR_FRONT_LEFT_PWM 6
+#define MOTOR_FRONT_LEFT_FACTOR -3
+
+//Mo1  bei Platine
+#define MOTOR_FRONT_RIGHT_IN1 0
+#define MOTOR_FRONT_RIGHT_IN2 1
+#define MOTOR_FRONT_RIGHT_PWM 4
+#define MOTOR_FRONT_RIGHT_FACTOR 1
+
+//-------Enums---------
+
+enum Buttons
+{
+    BUTTON1 = 4,
+    BUTTON2 = 5,
+    BUTTON3 = 6,
+    BUTTON4 = 7,
+    BUTTON5 = 8,
+    BUTTON6 = 9
+};
+enum Switch{
+    SWITCH9 = 0,
+    SWITCH10 = 1,
+    SWITCH11 = 2,
+    SWITCH12 = 3,
+    StartSwitch
+};
+
+enum Mutliplexer{
+    MULTIPLEXER_TOP_1,
+    MULTIPLEXER_TOP_2,
+    MULTIPLEXER_BOTTOM_1,
+    MULTIPLEXER_BOTTOM_2,
+};
+
+enum TOF{
+    FRONTRIGHT,
+    FRONTLEFT,
+    RIGHTFRONT,
+    RIGHTBACK,
+    LEFTFRONT,
+    LEFTBACK,
+    BACKRIGHT,
+    BACKLEFT
+};
+
+enum SkyDirection{
+    North,
+    East,
+    South,
+    West
+};
--- lib/General/General.h
+++ lib/General/General.h
@@ -1,6 +1,8 @@
 #ifndef General_h
 #define General_h
 
+//Defines & Enums
+#include "DefineEnum.h"
 //Display
 #include "../Display/Adafruit_ILI9341-master/Adafruit_ILI9341.h"
 //Expander
@@ -8,79 +10,11 @@
 //Motors
 #include "../Motors/Motor/Motor.h"
 #include "../Motors/Movement/Movement.h"
-
-#define MOTOR_FREQUENCY 150
-
-//Mo2 bei Platine
-#define MOTOR_BACK_LEFT_IN1 2
-#define MOTOR_BACK_LEFT_IN2 3
-#define MOTOR_BACK_LEFT_PWM 5
-#define MOTOR_BACK_LEFT_FACTOR -1
-
-//Mo4  bei Platine
-#define MOTOR_BACK_RIGHT_IN1 6
-#define MOTOR_BACK_RIGHT_IN2 7
-#define MOTOR_BACK_RIGHT_PWM 9
-#define MOTOR_BACK_RIGHT_FACTOR 5
-
-//Mo3 bei Platine
-#define MOTOR_FRONT_LEFT_IN1 4
-#define MOTOR_FRONT_LEFT_IN2 5
-#define MOTOR_FRONT_LEFT_PWM 6
-#define MOTOR_FRONT_LEFT_FACTOR -3
-
-//Mo1  bei Platine
-#define MOTOR_FRONT_RIGHT_IN1 0
-#define MOTOR_FRONT_RIGHT_IN2 1
-#define MOTOR_FRONT_RIGHT_PWM 4
-#define MOTOR_FRONT_RIGHT_FACTOR 1
 //BNO
 #include "../Adafruit_BNO055/Adafruit_BNO055.h"
 //TOF
 #include "../Adafruit_VL6180X-master/Adafruit_VL6180X.h"
-//------------------
 
-enum Buttons
-{
-    BUTTON1 = 4,
-    BUTTON2 = 5,
-    BUTTON3 = 6,
-    BUTTON4 = 7,
-    BUTTON5 = 8,
-    BUTTON6 = 9
-};
-enum Switch{
-    SWITCH9 = 0,
-    SWITCH10 = 1,
-    SWITCH11 = 2,
-    SWITCH12 = 3,
-    StartSwitch
-};
-
-enum Mutliplexer{
-    MULTIPLEXER_TOP_1,
-    MULTIPLEXER_TOP_2,
-    MULTIPLEXER_BOTTOM_1,
-    MULTIPLEXER_BOTTOM_2,
-};
-
-enum TOF{
-    FRONTRIGHT,
-    FRONTLEFT,
-    RIGHTFRONT,
-    RIGHTBACK,
-    LEFTFRONT,
-    LEFTBACK,
-    BACKRIGHT,
-    BACKLEFT
-};
-
-enum SkyDirection{
-    North,
-    East,
-    South,
-    West
-};
 
 class Robot{
     public:

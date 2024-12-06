c6d8944a14101328ddb9fa17c61f7327eeb1861b
Erik Bergmann <lese.erik@gmail.com>
2024-12-06 13:53:39 +0100

    TOFonMux doesn't inherit mux but has pointer to one

--- lib/TOF/TOF/TOF.h
+++ lib/TOF/TOF/TOF.h
@@ -5,16 +5,17 @@
 #include "../General/DefineEnum.h" //Warum will VScode nur 2 "."? Es sollten 3 sein!
 #include "../Portexpander/src/SparkFun_I2C_Mux_Arduino_Library.h"
 
-class TOFonMux : public QWIICMUX, public Adafruit_VL6180X{
+class TOFonMux : public Adafruit_VL6180X{
     public:
-    TOFonMux(uint8_t expanderAddress = QWIIC_MUX_DEFAULT_ADDRESS, TwoWire &wirePort = Wire, uint8_t TOFpin) : Adafruit_VL6180X() {
-        QWIICMUX::begin(expanderAddress, wirePort);
-        this->pin = TOFpin;
+    TOFonMux(uint8_t pinOnMux, uint8_t expanderAddress = QWIIC_MUX_DEFAULT_ADDRESS, TwoWire &wirePort = Wire) : Adafruit_VL6180X() {
+        expander->begin(expanderAddress, wirePort);
+        this->pin = pinOnMux;
     };
 
 
     private:
     uint8_t pin;
+    QWIICMUX *expander;
 
 };
 

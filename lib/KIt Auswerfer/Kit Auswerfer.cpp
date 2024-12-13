e45b233d8762480c5c710e3a681f5045c8b7e174
Hannes Falke <hannes11.falke@gmail.com>
2024-12-13 17:45:57 +0100

    added Kit auswerfer

---
+++ lib/KIt Auswerfer/Kit Auswerfer.cpp
@@ -0,0 +1,29 @@
+#include "Kit Auswerfer.h"
+#include <Servo.h>
+
+Servo myservo;
+
+int ServoPos = 0;
+int ServoResetPos = 90;
+int ServoSpeed = 15;
+
+
+void AuswurfR()     {
+    myservo.write(ServoResetPos);
+    for (ServoPos = ServoResetPos; ServoPos >= 10; ServoPos--)  {
+        myservo.write(ServoPos);
+        delay(ServoSpeed);
+    }
+}
+
+void AuswurfL()     {
+    myservo.write(ServoResetPos);
+    for (ServoPos = ServoResetPos; ServoPos >= 170; ServoPos++)  {
+        myservo.write(ServoPos);
+        delay(ServoSpeed);
+    }
+}
+
+void setupServo()    {
+    myservo.attach(9);      // hier muss man noch den Port anpassen, 9 kommt nur aus dem Beispielprogramm
+}
---
+++ lib/KIt Auswerfer/Kit Auswerfer.h
@@ -0,0 +1,5 @@
+#ifndef kit
+#define kit
+
+
+#endif

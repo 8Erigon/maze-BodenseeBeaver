7e2e92a3bee76f6a5688ad6fe7b6e6dc535f8b75
Erik Bergmann <lese.erik@gmail.com>
2024-12-13 14:35:03 +0100

    TOFonMux automaticly switches port on mux

--- lib/TOF/TOF.cpp
+++ lib/TOF/TOF.cpp
@@ -3,9 +3,122 @@
 TOFonMux::TOFonMux(uint8_t pinOnMux, QWIICMUX *expander) : Adafruit_VL6180X() {
     this->expander = expander;
     this->pin = pinOnMux;
+    begin();
 };
 
 TOFonMux::TOFonMux(){
     this->expander = NULL;
     this->pin = 9; //Expander/Mux only has ports from 0-7
 };
+
+boolean TOFonMux::begin(TwoWire *theWire = &Wire) {
+    if(this->expander == NULL) {
+        return false;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::begin(theWire);
+};
+
+boolean TOFonMux::setAddress(uint8_t newAddr) {
+    if(this->expander == NULL) {
+        return false;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::setAddress(newAddr);
+}
+
+uint8_t TOFonMux::getAddress(void) {
+    if(this->expander == NULL) {
+        return 0;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::getAddress();
+}
+
+uint8_t TOFonMux::readRange(void) {
+    if(this->expander == NULL) {
+        return 0;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::readRange();
+}
+
+float TOFonMux::readLux(uint8_t gain) {
+    if(this->expander == NULL) {
+        return 0;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::readLux(gain);
+}
+
+uint8_t TOFonMux::readRangeStatus(void) {
+    if(this->expander == NULL) {
+        return 0;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::readRangeStatus();
+}
+
+boolean TOFonMux::startRange(void) {
+    if(this->expander == NULL) {
+        return false;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::startRange();
+}
+
+boolean TOFonMux::isRangeComplete(void) {
+    if(this->expander == NULL) {
+        return false;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::isRangeComplete();
+}
+
+boolean TOFonMux::waitRangeComplete(void) {
+    if(this->expander == NULL) {
+        return false;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::waitRangeComplete();
+}
+
+uint8_t TOFonMux::readRangeResult(void) {
+    if(this->expander == NULL) {
+        return 0;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::readRangeResult();
+}
+
+void TOFonMux::startRangeContinuous(uint16_t period_ms) {
+    if(this->expander == NULL) {
+        return;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::startRangeContinuous(period_ms);
+}
+
+void TOFonMux::stopRangeContinuous(void) {
+    if(this->expander == NULL) {
+        return;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::stopRangeContinuous();
+}
+
+void TOFonMux::setOffset(uint8_t offset) {
+    if(this->expander == NULL) {
+        return;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::setOffset(offset);
+}
+
+void TOFonMux::getID(uint8_t *id_ptr) {
+    if(this->expander == NULL) {
+        return;
+    }
+    this->expander->setPort(this->pin);
+    return Adafruit_VL6180X::getID(id_ptr);
+}
--- lib/TOF/TOF.h
+++ lib/TOF/TOF.h
@@ -5,10 +5,30 @@
 #include "../General/DefineEnum.h" 
 #include "../Portexpander/src/SparkFun_I2C_Mux_Arduino_Library.h"
 
-class TOFonMux : public Adafruit_VL6180X{
+class TOFonMux : virtual public Adafruit_VL6180X{
     public:
     TOFonMux(uint8_t pinOnMux, QWIICMUX *expander);
     TOFonMux();
+
+    boolean begin(TwoWire *theWire = &Wire) override;
+    boolean setAddress(uint8_t newAddr) override;
+    uint8_t getAddress(void) override;
+
+    uint8_t readRange(void) override;
+    float readLux(uint8_t gain) override;
+    uint8_t readRangeStatus(void) override;
+
+    boolean startRange(void) override;
+    boolean isRangeComplete(void) override;
+    boolean waitRangeComplete(void) override;
+    uint8_t readRangeResult(void) override;
+
+    void startRangeContinuous(uint16_t period_ms = 50) override;
+    void stopRangeContinuous(void) override;
+    // readRangeResult and isRangeComplete apply here is well
+
+    void setOffset(uint8_t offset) override;
+    void getID(uint8_t *id_ptr) override;
 
     uint8_t range;
     private:

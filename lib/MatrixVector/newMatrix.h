f3a8824f5fe0c5600aa303806b43ac0a86672315
Erik Bergmann <lese.erik@gmail.com>
2024-12-22 23:56:32 +0100

    fixed spelling of Height

--- lib/MatrixVector/newMatrix.h
+++ lib/MatrixVector/newMatrix.h
@@ -1,39 +1,39 @@
 #ifndef Matrix_h
 #define Matrix_h
 
-template <typename type, int hight, int width> class Matrix{
+template <typename type, int height, int width> class Matrix{
     #if (type != int && type != long && type != float && type != double) //throw error if type is not a number when compiling
     #error "The class 'Matrix' only allows a number type like int."
     #endif
     public: 
-    type content[hight][width];
+    type content[height][width];
 
-    Matrix<type, hight, width> operator=(const type other[hight][width]){ //function for when you do "matrix = 2dArray[][]"
-        for(int i; i<hight; i++){
+    Matrix<type, height, width> operator=(const type other[height][width]){ //function for when you do "matrix = 2dArray[][]"
+        for(int i; i<height; i++){
             for(int j; j<width; j++){
                 this->content[i][j] = other[i][j];
             }
         }
         return *this;
     }
-    Matrix<type, hight, width> operator=(const Matrix<type, hight, width> other){ //function for when you do "matrix = Matrix"
+    Matrix<type, height, width> operator=(const Matrix<type, height, width> other){ //function for when you do "matrix = Matrix"
         *this = other.content;
         return *this;
     }
 
 
-    Matrix<type, hight, width> operator*(const double &other){ //function for when you do "matrix * int"
-        Matrix<type, hight, width> result;
-        for(int i; i<hight; i++){
+    Matrix<type, height, width> operator*(const double &other){ //function for when you do "matrix * int"
+        Matrix<type, height, width> result;
+        for(int i; i<height; i++){
             for(int j; j<width; j++){
                 result.content[i][j] = content[i][j] * other;
             }
         }
         return result;
     }
-    Matrix<type, hight, width> operator/(const double &other){ //function for when you do "matrix / int"
-        Matrix<type, hight, width> result;
-        for(int i; i<hight; i++){
+    Matrix<type, height, width> operator/(const double &other){ //function for when you do "matrix / int"
+        Matrix<type, height, width> result;
+        for(int i; i<height; i++){
             for(int j; j<width; j++){
                 result.content[i][j] = content[i][j] / other;
             }
@@ -41,18 +41,18 @@ template <typename type, int hight, int width> class Matrix{
         return result;
     }
 
-    Matrix<type, hight, width> operator+(const Matrix<type, hight, width> &other){ //function for when you do "matrix + matrix"
-        Matrix<type, hight, width> result;
-        for(int i; i<hight; i++){
+    Matrix<type, height, width> operator+(const Matrix<type, height, width> &other){ //function for when you do "matrix + matrix"
+        Matrix<type, height, width> result;
+        for(int i; i<height; i++){
             for(int j; j<width; j++){
                 result.content[i][j] = content[i][j] + other.content[i][j];
             }
         }
         return result;
     }
-    Matrix<type, hight, width> operator-(const Matrix<type, hight, width> &other){ //function for when you do "matrix - matrix"
-        Matrix<type, hight, width> result;
-        for(int i; i<hight; i++){
+    Matrix<type, height, width> operator-(const Matrix<type, height, width> &other){ //function for when you do "matrix - matrix"
+        Matrix<type, height, width> result;
+        for(int i; i<height; i++){
             for(int j; j<width; j++){
                 result.content[i][j] = content[i][j] - other.content[i][j];
             }

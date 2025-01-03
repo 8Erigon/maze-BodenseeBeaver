249bb4ec228de9e00138b49d22197167ad00aa55
Erik Bergmann <lese.erik@gmail.com>
2025-01-03 17:49:31 +0100

    new main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,83 +5,22 @@
 //  put function declarations here:
 #define Speed 15
 
+uint32_t color = 0;
 int main()
 {
   Serial.begin(9600);
+  Serial.println("---Begin programm---");
   Robot robo = Robot();
-  // Motor motors[4] ;
+  Serial.println("---Robo initialized---");
 
   robo.setRunning(false);
-  while (true)
-  {
-    robo.process();
-    Serial.println(robo.orientation.heading);
-    Serial.println(robo.orientation.pitch);
-    Serial.println(robo.orientation.roll);
-    Serial.println();
-    // robo.move.TurnRight(20);
-    // robo.move.TurnLeft(20);
-    // robo.move.ForwardOneTile(15);
-    if (robo.readSwitch(StartSwitch))
-    {
-      robo.move.ForwardOneTile(20);
+  robo.setLedColor(OFF);
 
-      float RoboHeading = robo.orientation.roll;
-      float RoboHeadingNew = robo.orientation.roll;
-      float RoboHeadingFinal = RoboHeading + 90;
-
-      Serial.println(RoboHeading);
-
-      if (RoboHeadingFinal < 360)
-      {
-
-        while (RoboHeadingNew < RoboHeadingFinal)
-        {
-          robo.process();
-          robo.motors[0].speed = Speed;
-          robo.motors[1].speed = Speed;
-          robo.motors[2].speed = -Speed;
-          robo.motors[3].speed = -Speed;
-          robo.motors[0].processOutput();
-          robo.motors[1].processOutput();
-          robo.motors[2].processOutput();
-          robo.motors[3].processOutput();
-          delay(100);
-          robo.move.Stop(10);
-
-          RoboHeadingNew = robo.orientation.roll;
-          Serial.println(RoboHeadingNew);
-          // delay(TurnDuration/Speed);
-        }
-      }
-      else
-      {
-        RoboHeadingFinal = RoboHeadingFinal - 360;
-        while (RoboHeadingNew > RoboHeadingFinal)
-        {
-          robo.process();
-          robo.motors[0].speed = Speed;
-          robo.motors[1].speed = Speed;
-          robo.motors[2].speed = -Speed;
-          robo.motors[3].speed = -Speed;
-          robo.motors[0].processOutput();
-          robo.motors[1].processOutput();
-          robo.motors[2].processOutput();
-          robo.motors[3].processOutput();
-          delay(10);
-          robo.move.Stop(1);
-
-          RoboHeadingNew = robo.orientation.roll;
-          Serial.println(RoboHeadingNew);
-          // delay(TurnDuration/Speed);
-        }
-      }
-      robo.move.Stop(200);
-    }
-    // robo.move.TurnRight(10);
-    // motors[0].speed = 20;
-    // delay(1000);
-    robo.move.Stop(1000);
+  while (true){
+    //robo.process();
+    robo.TOF[TOF_LEFTBACK].range = robo.TOF[TOF_LEFTBACK].readRange();
+    Serial.println(robo.TOF[TOF_LEFTBACK].range);
+    delay(100);
   }
 }
 

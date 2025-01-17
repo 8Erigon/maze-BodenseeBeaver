04fcd73a6869672b98d6a0aa79150ac65cb78e72
Erik Bergmann <lese.erik@gmail.com>
2025-01-17 17:43:30 +0100

    wtf did I have to correct

--- lib/TOF/TOF.h
+++ lib/TOF/TOF.h
@@ -1,11 +1,11 @@
 #ifndef TOF_h
 #define TOF_h
 
-#include "C:\Users\thoma\Beaver repository\maze-milbe-test-1\lib\Adafruit_VL6180X\Adafruit_VL6180X.h"
+#include "../Adafruit_VL6180X/Adafruit_VL6180X.h"
 #include "../General/DefineEnum.h" 
 #include "../Portexpander/src/SparkFun_I2C_Mux_Arduino_Library.h"
 
-class TOFonMux : virtual public Adafruit_VL6180X{
+class TOFonMux : public Adafruit_VL6180X{
     public:
     TOFonMux(uint8_t pinOnMux, QWIICMUX &expander, TwoWire &I2C_bus = Wire);
     TOFonMux();

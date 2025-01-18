c6a7f88a7fa61ff83fd44d152ebdf2ac86441659
Erik Bergmann <lese.erik@gmail.com>
2025-01-18 16:26:47 +0100

    +

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,193 @@ float RoboHeading = 0;
 float RoboHeadingNew = 0;
 float RoboHeadingFinal = 0;
 
-bool processIsRunning = false;
+
 bool isfirstRun = true;
+
+
+
+//MazeMap
+bool robotCompletedRightWallDrivePath = false;
+
+// Switches
+bool startSwitchIsOn = false;
+
+bool SingleStepModeSwitchIsOn = false;
+bool debugWindowSwitchIsOn = false;
+bool SW11 = false;
+bool SW12 = false;
+
+// Buttons
+bool resetCycleTimeButtonIsPressed = false;
+bool resetChainButtonIsPressed = false;
+bool singleStepButtonIsPressed = false;
+bool cycleDebugWindowButtonIsPressed = false;
+bool BTN5 = false;
+bool mazeMapPrintButtonIsPressed = false;
+
+// VL Tofs
+uint8_t distanceFrontRightSide = 0;
+uint8_t distanceFrontLeftSide = 0;
+uint8_t averageDistanceFront = 0;
+
+uint8_t distanceRightFrontSide = 0;
+uint8_t distanceRightBackSide = 0;
+uint8_t averageDistanceRight = 0;
+
+uint8_t distanceBackRightSide = 0;
+uint8_t distanceBackLeftSide = 0;
+uint8_t averageDistanceBack = 0;
+
+uint8_t distanceLeftFrontSide = 0;
+uint8_t distanceLeftBackSide = 0;
+uint8_t averageDistanceLeft = 0;
+
+bool disableVlTofs = false;
+
+//Lanbao Tofs
+uint16_t distanceFrontMiddle = false;
+uint16_t distanceRightMiddle = false;
+uint16_t distanceBackMiddle = false;
+uint16_t distanceLeftMiddle = false;
+bool disableLanbaoTofs = false;
+
+// Colorsenor
+uint16_t red = 0;
+uint16_t blue = 0;
+uint16_t green = 0;
+uint16_t clear = 0;
+bool disableColorSensor = false;
+
+// Walls
+bool hasWallFront = false;
+bool hasWallBack = false;
+bool hasWallRight = false;
+bool hasWallLeft = false;
+
+// Ramps, stairs and bumper
+bool hasStairUp = false;
+bool hasBumper = false;
+unsigned long rampTime = 0;                       // Unit ; µs : 0µs
+unsigned long stairTime = 0;                       // Unit ; µs : 0µs
+
+// Bno
+float robotHeading = 0;
+float robotPitch = 0;
+float bnoOffsetCalbHeading = 0;
+float bnoOffsetCalbPitch = 0;
+float bnoCalbHeading = 0;
+float bnoCalbPitch = 0;
+float angleTarget = 0;
+float robotAccX = 0;
+float robotAccY = 0;
+float robotAccZ = 0;
+
+float last100PitchValues[100] = {};
+int currentLastPitchValue = 0;
+
+// Color Sensor
+bool isOnBlackTile = false;
+bool isOnBlueTile = false;
+bool isOnControllTile = false;
+plateTypes currentTileType = CHECKPOINT;
+
+//Display
+bool disableDisplay = false;
+
+// Leds
+bool victimLedMode = false;
+bool startLedMode = false;
+
+bool LifeClockLEDIsOn = false;
+
+// Victims
+bool victimHarm = false;
+bool victimStable = false;
+bool victimRed = false;
+bool victimYellow = false;
+bool hasVictim = false;
+char leftCameraResponse = 'X';
+char rightCameraResponse = 'X';
+victimTypes victimForMap;
+
+// Servo
+int servoPos = 90;
+
+// StepChain
+int step = 0;
+int lastStep = 0;
+int nextStep = 0;
+int stepToJumpBackTo = -1;
+bool isFirstRun = true;
 bool stepHasChanged = false;
+bool processIsRunning = false;
 bool singleStepFlag = false;
 bool singleStep = false;
+bool chainFlag = false;
+bool chainFlagAngleTarget = false;
+bool ignoreNextPlateAddition = false;
 
-int lastStep = 0;
+//Debug
+bool debugWindowFlag = false;
 
-int step = 0;
+// Correction
+float degreeOffset = 0;
+float angleToFixSideWallDistance = 0; 
+// PID
+// Rotation PID
+// Final
+double proportionalConstantForRotation = 2.0;
+double integralConstantForRotation = 22.0;
+double differentialConstantForRotation = 20.0;
+
+int maxIntegralValueForRotation = 170;
+int maxOutputSpeedForRotation = MAX_ROBOT_ROTATION_SPEED;
+int minOutputSpeedForRotation = 32;
+
+double degreeAccuracyForRotation = 0.6;
+double speedAccuracyForRotation = 40.0;
+double integralNewPointForRotation = 30.0;
+
+// Programm Input
+bool doPIDForRotation = false;
+float degreesToRotateForRotation = 0;
+
+// Processing
+double bnoSetpointForRotation = 0;
+double bnoOffsetForRotation = 0;
+double errorForRotation = 0;
+
+double previousIntegralForRotation = 0;
+double previouserrorForRotation = 0;
+double previousmessungForRotation = 0;
+
+double differenceMeasurementForRotation = 0;
+double currentRotationForRotation = 0;
+
+// Output
+double outputSpeedForRotation = 0;
+
+
+// Drive PID
+bool doDrivePID = false;
+
+double bnoSetpointForDrive = 0;
+double bnoOffsetForDrive = 0;
+
+double errorForDrive = 0;
+double previousErrorForDrive = 0;
+double integralForDrive = 0;
+double previousIntegralForDrive = 0;
+double proportionalForDrive = 0;
+
+double maxIntegralValueForDrive = 60;
+double maxOutputValueDifferenceForDrive = 70;
+
+double integralNewPointForDrive = 3;
+
+double integralConstantForDrive = 60;
+double proportionalConstantForDrive = 120;
+// End PID
 
 int main()
 {
